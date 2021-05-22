e9ce792e05598265ad7848bb2aa5f221ffc96254
danielvcorreia <danielvalacorreia@ua.pt>
2021-05-23 00:27:18 +0100

    Added assignment-1 files

---
+++ ASS1_T1_G7/AirLiftVConc/src/entities/package-info.java
@@ -0,0 +1,6 @@
+/**
+ *  Definition of the entities intervening in the Air Lift.
+ *  Static solution based on a posteriori reasoning to terminate the pilot and hostess threads.
+ */
+
+package entities;
