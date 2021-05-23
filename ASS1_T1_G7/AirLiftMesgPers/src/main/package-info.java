98a9577871a6a6f24be1636c10be5dfdc19e2a14
danielvcorreia <danielvalacorreia@ua.pt>
2021-05-23 01:57:44 +0100

    Some changes in working dir's

---
+++ ASS1_T1_G7/AirLiftMesgPers/src/main/package-info.java
@@ -0,0 +1,6 @@
+/**
+ *  Static solution of the Air Lift.
+ *  Synchronization is based on implicit monitors.
+ */
+
+package main;
---
+++ ASS1_T1_G7/AirLiftMesgPers/src/sharedRegions/package-info.java
@@ -0,0 +1,6 @@
+/**
+ *  Definition of the shared regions in the Air Lift.
+ *  Static solution with implicit monitors based synchronization.
+ */
+
+package sharedRegions;
