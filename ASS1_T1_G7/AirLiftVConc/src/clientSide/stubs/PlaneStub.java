4e6398c1ee24df2a831afb1085fd5f606a346694
danielvcorreia <danielvalacorreia@ua.pt>
2021-05-24 04:10:20 +0100

    Added .gitignore and some things on PlaneStub.java

--- ASS1_T1_G7/AirLiftVConc/src/clientSide/stubs/PlaneStub.java
+++ ASS1_T1_G7/AirLiftVConc/src/clientSide/stubs/PlaneStub.java
@@ -1,4 +1,39 @@
 package clientSide.stubs;
 
+/**
+ * Stub to the plane.
+ *
+ * It instantiates a remote reference to the plane.
+ * Implementation of a client-server model of type 2 (server replication).
+ * Communication is based on a communication channel under the TCP protocol.
+ */
+
 public class PlaneStub {
+
+    /**
+     *  Name of the platform where is located the plane server.
+     */
+
+    private String serverHostName;
+
+    /**
+     *  Port number for listening to service requests.
+     */
+
+    private int serverPortNumb;
+
+    /**
+     *  Instantiation of a stub to the plane.
+     *
+     *  @param serverHostName name of the platform where is located the barber shop server
+     *  @param serverPortNumb port number for listening to service requests
+     */
+
+    public PlaneStub (String serverHostName, int serverPortNumb)
+    {
+        this.serverHostName = serverHostName;
+        this.serverPortNumb = serverPortNumb;
+    }
+
+
 }
