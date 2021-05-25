fc3828a854a72559530f0516a0a29c5dcd0dd52a
danielvcorreia <danielvalacorreia@ua.pt>
2021-05-25 20:40:28 +0100

    Deleted assignment-1 files

--- ASS1_T1_G7/AirLiftVConc/src/entities/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- *  Definition of the entities intervening in the Air Lift.
- *  Static solution based on a posteriori reasoning to terminate the pilot and hostess threads.
- */
-
-package entities;
--- ASS1_T1_G7/AirLiftVConc/src/sharedRegions/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- *  Definition of the shared regions in the Air Lift.
- *  Static solution with implicit monitors based synchronization.
- */
-
-package sharedRegions;
