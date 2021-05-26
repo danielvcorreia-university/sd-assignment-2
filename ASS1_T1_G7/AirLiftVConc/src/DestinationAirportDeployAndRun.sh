2ed668cf71a85b0a2ffcda19d379a57884fa688a
Alexandre Abreu <alexandre.abreu@ua.pt>
2021-05-26 19:34:46 +0100

    Password and shell fix.

--- ASS1_T1_G7/AirLiftVConc/src/DestinationAirportDeployAndRun.sh
+++ ASS1_T1_G7/AirLiftVConc/src/DestinationAirportDeployAndRun.sh
@@ -1,8 +1,8 @@
 echo "Transfering data to the destination airport node."
-sshpass -f ../password ssh sd104@l040101-ws03.ua.pt 'mkdir -p ~/destinationairport'
-sshpass -f ../password ssh sd104@l040101-ws03.ua.pt 'rm -rf ~/destinationairport/*'
-sshpass -f ../password scp destinationairport.zip sd104@l040101-ws03.ua.pt:~/destinationairport/
+sshpass -f password ssh sd104@l040101-ws03.ua.pt 'mkdir -p ~/destinationairport'
+sshpass -f password ssh sd104@l040101-ws03.ua.pt 'rm -rf ~/destinationairport/*'
+sshpass -f password scp destinationairport.zip sd104@l040101-ws03.ua.pt:~/destinationairport/
 echo "Decompressing data sent to the destination airport node."
-sshpass -f ../password ssh sd104@l040101-ws03.ua.pt 'cd ~/destinationairport/ ; unzip -uq destinationairport.zip'
+sshpass -f password ssh sd104@l040101-ws03.ua.pt 'cd ~/destinationairport/ ; unzip -uq destinationairport.zip'
 echo "Executing program at the destination airport node."
 sshpass -f password ssh sd104@l040101-ws03.ua.pt 'cd ~/destinationairport/ ; java -cp .:genclass.jar serverSide.main.DestinationAirportMain
--- ASS1_T1_G7/AirLiftVConc/src/PlaneDeployAndRun.sh
+++ ASS1_T1_G7/AirLiftVConc/src/PlaneDeployAndRun.sh
@@ -1,8 +1,8 @@
 echo "Transfering data to the plane node."
-sshpass -f ../password ssh sd104@l040101-ws02.ua.pt 'mkdir -p ~/plane'
-sshpass -f ../password ssh sd104@l040101-ws02.ua.pt 'rm -rf ~/plane/*'
-sshpass -f ../password scp plane.zip sd104@l040101-ws02.ua.pt:~/plane/
+sshpass -f password ssh sd104@l040101-ws02.ua.pt 'mkdir -p ~/plane'
+sshpass -f password ssh sd104@l040101-ws02.ua.pt 'rm -rf ~/plane/*'
+sshpass -f password scp plane.zip sd104@l040101-ws02.ua.pt:~/plane/
 echo "Decompressing data sent to the plane node."
-sshpass -f ../password ssh sd104@l040101-ws02.ua.pt 'cd ~/plane/ ; unzip -uq plane.zip'
+sshpass -f password ssh sd104@l040101-ws02.ua.pt 'cd ~/plane/ ; unzip -uq plane.zip'
 echo "Executing program at the plane node."
 sshpass -f password ssh sd104@l040101-ws02.ua.pt 'cd ~/plane/ ; java -cp .:genclass.jar serverSide.main.PlaneMain
